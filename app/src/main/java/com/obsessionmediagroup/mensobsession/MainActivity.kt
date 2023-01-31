f75036db890d81b58b7eb61f287f9bfdc6665c09
Dimas Putra Widiyanto <putrad322@gmail.com>
2023-01-31 16:12:28 +0700

    Changed app icon

--- app/src/main/java/com/obsessionmediagroup/mensobsession/MainActivity.kt
+++ app/src/main/java/com/obsessionmediagroup/mensobsession/MainActivity.kt
@@ -1,11 +1,43 @@
 package com.obsessionmediagroup.mensobsession
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowInsetsCompat
 
+@SuppressLint("StaticFieldLeak")
+private lateinit var webView: WebView
+
+@SuppressLint("SetJavaScriptEnabled")
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        webView = findViewById(R.id.mo_webView)
+
+        WindowInsetsCompat.Type.navigationBars()
+
+        webView.webViewClient = WebViewClient()
+
+        webView.loadUrl(WEB_URL)
+
+        webView.settings.javaScriptEnabled = true
+
+        webView.settings.setSupportZoom(true)
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    companion object {
+        private const val WEB_URL = "https://www.mensobsession.com/"
     }
 }
